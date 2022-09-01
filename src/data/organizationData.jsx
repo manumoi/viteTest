04cbb8b26542bd52b9932e6c3a43c716ab4522a9
Emmanuel Blanchard <emmanuel.g.blanchard@gmail.com>
2022-09-01 16:29:31 -0400

    update of data consts

--- src/data/organizationData.jsx
+++ src/data/organizationData.jsx
@@ -1,6 +1,26 @@
 const organizationDataRows = [
-    { id: 28, name: "Musée d'Art Contemporain des Laurentides", code: "MACLAU", presentation: "ce musée est situé à saint jérome" },
-
+    { _id: "o1", name: "Musée d'Art Contemporain des Laurentides", code: "MACLAU", access: "maclau", presentation: "ce musée est situé à saint jérome" },
+    { _id: "o2", name: "Musée d'Art Contemporain des Laurentides", code: "IDU", access: "idu", presentation: "ce musée est situé à saint jérome" },
 ]
 
-export { organizationDataRows }
+const contentDataRows = [
+    {
+        _id: "c1", orgaId: "o1", name: 'Rallye Famille FR', presentation: "blablabla, ",
+        longitude: 0.0,
+        latitude: 0.0,
+        code: "bla ble bli",
+        variants: [
+            { _id: "v1", name: 'Titre de la v1', description: "description de la v1", url: "url de la v1", imgUrl: "imgUrl de la v1" },
+            { _id: "v2", name: 'Titre de la v2', description: "description de la v2", url: "url de la v2", imgUrl: "imgUrl de la v2" },
+            { _id: "v3", name: 'Titre de la v3', description: "description de la v3", url: "url de la v3", imgUrl: "imgUrl de la v3" },
+        ],
+    },
+]
+
+
+
+
+
+
+
+export { organizationDataRows, contentDataRows }
